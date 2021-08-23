a239f347397711b2fbff56ade1f6b5b1d21100d0
Tester_No_test <38783518+Ghost-ate-your-brains@users.noreply.github.com>
2021-08-23 07:11:20 +0300

    Update unixic.sh

--- unixic.sh
+++ unixic.sh
@@ -1,6 +1,6 @@
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install git gnupg flex bison gperf build-essential \
+sudo apt install -y git gnupg flex bison gperf build-essential \
 zip bzr curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
 libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
 libgl1-mesa-dev g++-multilib mingw-w64-i686-dev tofrodos \
@@ -15,7 +15,7 @@ chmod a+rx ~/bin/repo
 mkdir halium-10.0 && cd halium-10.0
 ~/bin/repo init -u https://github.com/Halium/android -b halium-10.0 --depth=1
 ~/bin/repo sync -c -j 16
-curl https://raw.githubusercontent.com/Halium/halium-devices/aa82ba147c27c7d2ede8b267b14ab2a93df42bea/manifests/xiaomi_olive.xml > halium/devices/manifests/xiaomi_olive.xml
+curl https://raw.githubusercontent.com/Halium/halium-devices/aa82ba147c27c7d2ede8b267b14ab2a93df42bea/manifests/xiaomi_olive.xml > ~/halium-10.0/halium/devices/manifests/xiaomi_olive.xml
 ~/halium-10.0/halium/devices/setup olive
 ~/halium-10.0/hybris-patches/apply-patches.sh --mb
 source ~/halium-10.0/build/envsetup.sh
